f7797799f93fa861451def8562e2bc36e52289f0
destiny <desphixs@gmail.com>
2026-05-15 00:01:39 +0100

    integrated category filtering

--- quotes/views.py
+++ quotes/views.py
@@ -13,16 +13,27 @@ class QuoteList(APIView):
     # The 'get' method handles "GET" requests, which are used to "get" data.
     # Analogy: This is like a customer saying "Can I see the menu, please?"
     def get(self, request):
-        # 1. First, we go to the database and grab every single Quote we have.
-        # Analogy: This is like the waiter gathering all the menus from the kitchen.
+        # 1. We check if the user has asked for a specific "category" in the web address.
+        # 'request.query_params' is like a dictionary of all the extra details in the URL.
+        # Analogy: This is like a customer saying, "I'd like to see the menu, but 
+        # only the 'Desserts' section please."
+        category = request.query_params.get('category')
+
+        # 2. We start by looking at all our quotes, but we haven't fetched them yet.
         quotes = Quote.objects.all()
+
+        # 3. If the user DID provide a category, we narrow down our search.
+        if category:
+            # We use '.filter' to only pick the quotes that match the category.
+            # 'category__iexact' means "match the category name exactly, but ignore 
+            # if it's uppercase or lowercase (case-insensitive)".
+            # Analogy: This is like the waiter flipping through the menus and 
+            # only picking out the ones that are labeled 'Desserts'.
+            quotes = quotes.filter(category__iexact=category)
         
-        # 2. Next, we pass those quotes into our serializer (the translator).
-        # We set 'many=True' because we are sending a LIST of quotes, not just one.
-        # Analogy: This is like the waiter handing the menus to a translator 
-        # so they can be written in a language the customer understands.
+        # 4. Next, we pass our (possibly filtered) quotes into our serializer (the translator).
+        # We set 'many=True' because we are sending a LIST of quotes.
         serializer = QuoteSerializer(quotes, many=True)
         
-        # 3. Finally, we return the translated data as a "Response".
-        # Analogy: The waiter brings the translated menus back to the customer's table.
+        # 5. Finally, we return the translated data as a "Response".
         return Response(serializer.data)
